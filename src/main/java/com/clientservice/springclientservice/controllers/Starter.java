caa8bdf0b0247025d13307ef3bba5af1078781e7
123VladimiR123 <888prikol888@gmail.com>
2024-05-03 11:02:07 +0300

    try to fix statics

--- src/main/java/com/clientservice/springclientservice/controllers/Starter.java
+++ src/main/java/com/clientservice/springclientservice/controllers/Starter.java
@@ -7,19 +7,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/")
 public class Starter {
 
     @Value("${custom.gateway.address}")
     private String prefix;
     private final ProfileRepository profileRepository;
 
-    @GetMapping("/")
+    @GetMapping
     public void starter(HttpServletResponse response,
                           HttpServletRequest request) throws IOException {
         final ProfileEntity profile = profileRepository.findByEmail(request.getHeader("email"));
